740703e33b113d78d3f56aca16bab98b9d2f65aa
Caleb Steinwandt <jsteinca@jci.com>
2025-05-26 13:57:35 -0600

    build: begin login page

--- groceryController.js
+++ groceryController.js
@@ -3,7 +3,7 @@ const db = require('./db/queries');
 exports.groceryListGet = async (req, res) => {
     const categories = await db.getCategories();
     const items = await db.getItems();
-    res.render("index", {categories: categories, items: items})
+    res.render("groceries", {categories: categories, items: items})
 }
 
 exports.addItemPost = async (req, res) => {
