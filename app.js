82c28a005759fa375ad0b97d0abd8e3b55b4d7d2
cabelstein12 <Caleb.Steinwandt@gmail.com>
2025-05-28 10:45:52 -0600

    toggle info on text input focus add home anchor to each page

--- app.js
+++ app.js
@@ -12,11 +12,12 @@ app.use(express.urlencoded({extended: true}));
 const assetsPath = path.join(__dirname, "public");
 
 app.use(express.static(assetsPath));
-app.use('/groceries', groceryRouter);
-
 app.get('/', (req,res) => {
     res.render("index.ejs")
 })
+
+app.use('/groceries', groceryRouter);
+
 
 app.get('/calendar', (req, res) => {
     res.render("calendar", {URL:'https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=America%2FEdmonton&showPrint=0&showTitle=0&src=MDkxNGI4N2NmOWIyMTc4ZmZkOTJkYzJlMDliMzc0YTVkYTFhZjRiNmQ3MmRiZjk1N2Q2ZWZjNWM3MTNlNzk2OEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AA'})
